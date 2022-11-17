de7520febf72caa2314da89c1ee07e817ff15012
PlatinuPL <platont@interia.pl>
2022-11-17 10:42:36 +0100

    Adding dummy data to the "Vievs" file

--- my_page/blog/views.py
+++ my_page/blog/views.py
@@ -4,6 +4,40 @@ from django.urls import reverse
 from django.template.loader import render_to_string
 
 
+posts = [
+    {
+        "slug":"hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "David",
+        "date": date(2022, 11, 17),
+        "title": "My biggest dream",
+        "excerpt": '''I will tell you the story of my biggest dream in life.
+                    There's nothing like the views you get when hiking in the
+                    mountains! And I wasn't even prepared for what happened whilst I
+                    was enjoying the view!'''
+        "content":'''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.'''
+    }
+]
+
+
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
@@ -12,6 +46,5 @@ def index(request):
 def posts(request):
     return render(request, "blog/posts.html")
 
-def single_post(request):
+def single_post(request,slug):
     return render(request, "blog/single_post.html")
-    
