69e9b283509e1a763599de9f1c9ecbdc00ff2d2c
PlatinuPL <platont@interia.pl>
2022-11-17 13:10:47 +0100

    Addind a sngle post and 404 pages

--- my_page/blog/views.py
+++ my_page/blog/views.py
@@ -2,19 +2,81 @@ from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
+from datetime import date
 
 
-posts = [
+all_posts = [
     {
         "slug":"hike-in-the-mountains",
-        "image": "mountains.jpg",
+        "image": "woods.jpg",
         "author": "David",
         "date": date(2022, 11, 17),
         "title": "My biggest dream",
         "excerpt": '''I will tell you the story of my biggest dream in life.
                     There's nothing like the views you get when hiking in the
                     mountains! And I wasn't even prepared for what happened whilst I
-                    was enjoying the view!'''
+                    was enjoying the view!''',
+        "content":''' My biggest dream Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.'''
+    },
+    
+       {
+        "slug":"Chips",
+        "image": "html.jpg",
+        "author": "David",
+        "date": date(2022, 11, 10),
+        "title": "My lays dream",
+        "excerpt": '''I hope its works''',
+        "content":''' My lays dream Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                    mollit anim id est laborum.'''
+    },
+    
+       {
+        "slug":"Snack",
+        "image": "snacks.jpg",
+        "author": "David",
+        "date": date(2022, 11, 1),
+        "title": "My peanut dream",
+        "excerpt": '''My peanut dream Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+                    ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, s''',
         "content":'''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
                     veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
@@ -37,14 +99,25 @@ posts = [
     }
 ]
 
+def get_date(post):
+    return post['date']
 
 # Create your views here.
 def index(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    lastes_posts = sorted_posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": lastes_posts
+    })
 
 
 def posts(request):
-    return render(request, "blog/posts.html")
+    return render(request, "blog/posts.html",{
+        "all_posts": all_posts
+    })
 
 def single_post(request,slug):
-    return render(request, "blog/single_post.html")
+    identified_post = next(post for post in all_posts if post["slug"]== slug)
+    return render(request, "blog/single_post.html", {
+        "post": identified_post
+    })
