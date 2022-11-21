9b8f946c8010e255ae94378a5b793c067a4f43e9
PlatinuPL <platont@interia.pl>
2022-11-21 17:31:21 +0100

    Relationships

--- bookstore/book_outlet/admin.py
+++ bookstore/book_outlet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address, Country
 # Register your models here.
 
 
@@ -13,9 +13,12 @@ class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug" : ("title",)}
 # dadnie filtrowanie w panelu Admin
     list_filter = ("author","rating",)
-
+# dadnie kolumn w panelu Admin
     list_display = ("title", "author",)
 
 
 
 admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
--- bookstore/book_outlet/models.py
+++ bookstore/book_outlet/models.py
@@ -4,12 +4,46 @@ from django.urls import reverse
 from django.utils.text import slugify
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name} - {self.code} "
+
+    class Meta:
+        verbose_name_plural = "Counties"
+
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.city} - {self.street} "
+
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null = True) # OnetoOne - przy relecjach jeden do jednego
+
+    # Ta funkcja determinuje jak klasa będzie wyświetlana jako string (np. w panelu administratora)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author = models.CharField(null = True, max_length=100)
+        #reprezentuje inny model (ForeignKey) CASCADE - usunięcie np. Authora usówa też książkę któą napisał
+        #inna opcja to PROTECT, która nie usówa książki mimo ze autor został usunięty. SET_NULL - zostawia pustę po usunieeciu authora
+    author = models.ForeignKey(Author, on_delete = models.CASCADE, null = True) # ForeignKey - przy relecjach wiele do jednego
     is_bestselling = models.BooleanField(default=False)
+    published_country = models.ManyToManyField(Country) # ManyToManyField - przy relecjach wiele do wielu
     slug = models.SlugField(default="", blank =True, null=False, db_index=True) # Harry Potter 1 => harry-potter-1
      #blank = True - dzięki temu nie musisz wpisywać niczego przy
      #  dodawaniu książki w panelu administratora w Django
