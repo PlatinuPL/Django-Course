8c6864ec353def4e42d78524a0e431cb00d6f30b
PlatinuPL <platont@interia.pl>
2022-12-12 22:25:55 +0100

    Change forms to django inbuild support

--- my_page/reviews/views.py
+++ my_page/reviews/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-
+from .forms import ReviewForm
 # Create your views here.
 
 def review (request):
     if request.method == "POST":
-        entered_username = request.POST["username"]
-        if entered_username == "" and len(entered_username) >=100:
-            return render(request, "reviews/review.html", {
-                "has_error" : True})
-        print(entered_username)
-        return HttpResponseRedirect("thank-you")
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("thank-you")
+    form = ReviewForm()
+
     return render(request, "reviews/review.html", {
-        "has_error" : False})
+        "form": form})
 
 def thankyou(request):
     return render(request, "reviews/thanks.html")
