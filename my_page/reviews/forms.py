d1d5229fe464ab60fedc0355d17304b866154546
PlatinuPL <platont@interia.pl>
2022-12-19 10:33:17 +0100

    Add more form controls

--- my_page/reviews/forms.py
+++ my_page/reviews/forms.py
@@ -5,3 +5,5 @@ class ReviewForm(forms.Form):
         "required": "Your name must not be empty!",
         "max_length": "Please enter the shorter name!"
     }) # label zmienia nazwę z standardowej 
+    review_text = forms.CharField(label="Your Feed", widget=forms.Textarea, max_length=200)
+    rating = forms.IntegerField(label = "Your Rating", min_value = 1, max_value = 5)
