5948150eb94e5e472db22c76ebfef9d90a126644
PlatinuPL <platont@interia.pl>
2022-12-19 09:52:23 +0100

    Add form style

--- my_page/my_page/settings.py
+++ my_page/my_page/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [ 
-    BASE_DIR / "static"
+    BASE_DIR / "static",
+    "/reviews/static/"
     ]
 
 # Default primary key field type
