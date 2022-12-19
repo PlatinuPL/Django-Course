30b30b66355528209e186e7570bc2b692793205f
PlatinuPL <platont@interia.pl>
2022-12-19 15:11:20 +0100

    Add ModelForm and configure it

--- my_page/reviews/forms.py
+++ my_page/reviews/forms.py
@@ -1,9 +1,27 @@
 from django import forms    
+from .models import Review
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label = "Your name:", max_length=100, error_messages={
+#         "required": "Your name must not be empty!",
+#         "max_length": "Please enter the shorter name!"
+#     }) # label zmienia nazwę z standardowej 
+#     review_text = forms.CharField(label="Your Feed", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label = "Your Rating", min_value = 1, max_value = 5)
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label = "Your name:", max_length=100, error_messages={
-        "required": "Your name must not be empty!",
-        "max_length": "Please enter the shorter name!"
-    }) # label zmienia nazwę z standardowej 
-    review_text = forms.CharField(label="Your Feed", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label = "Your Rating", min_value = 1, max_value = 5)
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        #fields = ["user_name", "review_text", "rating"]
+        fields = "__all__"
+        #exclude = "owner_comment" - Jeżeli chcesz zrobic wyjątek
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty!",
+                "max_length": "Please enter a shorter name!"
+            }
+        }
