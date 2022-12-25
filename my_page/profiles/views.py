b8623e6f8855fddf95cd781cb461519b0f58d64f
PlatinuPL <platont@interia.pl>
2022-12-25 10:45:01 +0100

    Using Model fo Filefield

--- my_page/profiles/views.py
+++ my_page/profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 from .forms import ProfileForm
+from .models import UserProfile
 
 # Create your views here.
 def store_file(file):
@@ -25,8 +26,9 @@ class CreateProfileView(View):
         submitted_form = ProfileForm(request.POST, request.FILES)
         
         if submitted_form.is_valid():
-            store_file(request.FILES["image"])
-            return HttpResponseRedirect("/profiles")
+            profile = UserProfile(image = request.FILES["user_image"])
+            profile.save()
+            return HttpResponseRedirect("/profiles/")
         return render(request, "profiles/create_profile.html", {
         "form": submitted_form
         })
