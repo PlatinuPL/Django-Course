7fb4d1ef3900ba7e9db1bca717b7f38df1843b32
PlatinuPL <platont@interia.pl>
2023-01-01 23:10:03 +0100

    Add some visual changes

--- my_page/blog/views.py
+++ my_page/blog/views.py
@@ -42,13 +42,24 @@ class AllPostsView(ListView):
 
 
 class SinglePostView(View):
+    def is_stored_post(self,request,post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+        return is_saved_for_later
+        
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, "blog/single_post.html", context)
 
@@ -67,7 +78,8 @@ class SinglePostView(View):
                 "post": post,
                 "post_tags": post.tags.all(),
                 "comment_form": comment_form,
-                "comments": post.comments.all().order_by("-id")
+                "comments": post.comments.all().order_by("-id"),
+                "saved_for_later": self.is_stored_post(request, post.id)
             }
         return render(request, "blog/single_post.html", context)
 
@@ -98,8 +110,9 @@ class ReadLaterView(View):
         
         if post_id not in stored_posts:
             stored_posts.append(post_id)
-            request.session["stored_posts"] = stored_posts
-
+        else:
+            stored_posts.remove(post_id)
+        request.session["stored_posts"] = stored_posts
         return HttpResponseRedirect("/")
 
     # def get_context_data(self, **kwargs):
